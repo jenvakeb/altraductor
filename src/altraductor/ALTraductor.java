d7372cd5558b861bbcb283ed94a34317d8f3b934
shlopezm4 <shlopezm4@gmail.com>
2018-04-03 15:32:33 -0600

    Pretty Stable Version 2

---
+++ src/altraductor/ALTraductor.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package altraductor;
+
+import java.io.File;
+
+/**
+ *
+ * @author betio_000
+ */
+public class ALTraductor {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        String path = "C:/Users/betio_000/Documents/NetBeansProjects/ALTraductor/src/altraductor/Lexer.flex";
+        generarLexer(path);
+    }
+    
+    public static void generarLexer(String path){
+         File file = new File(path);
+         jflex.Main.generate(file);
+    }
+}
---
+++ src/altraductor/Token.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package altraductor;
+
+/**
+ * @author betio_000
+ */
+public enum Token {
+    suma, multiplicacion, division, resta, NUMERO, ERROR, ART_INDEF, ART_DEFI, 
+    SUS_ABSTRACT, SUS_COMUN, SUS_PROPIO, ADJ_POSESIV,ADJ_DEMOS,ADJ_CALIFI,ADJ_NUMCARDIN,ADJ_NUMORD,
+    ADJ_PROPERSO,ADJ_PROPOSESIV,ADJ_PROREFLEX,ADJ_PROINTERR,PREP_MOD,PREP_LUG,PREP_MOV,PREP_TIEM
+   ,SIG_PUNT,VERBO_PRESENT,VERBO_PASADOSIMP,VERBO_PASADOPARTICI,CONJ_COORADVERV,CONJ_COORCOPU,CONJ_COORDISYU,
+   CONJ_SUBORDISYU,CONJ_SUBORCAUS,CONJ_CONJSUBOCONSEC,CONJ_CONJSUBOCONCES,CONJ_CONJSUBOCOMPARA,
+   AUX_PAS, AUX_PRESEN;
+}
